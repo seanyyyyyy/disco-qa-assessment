5137deeb3942bbd9a26b3d189dc15be5b04a7e84
sean <szliaw@gmail.com>
2024-04-15 22:16:52 +1000

    add files slider check

--- tests/pricing-page.spec.ts
+++ tests/pricing-page.spec.ts
@@ -17,19 +17,32 @@ test('Pay Annually > Move Sliders > Get Started', async ({ page }) => {
   expect(prices[1]).toHaveText('$13.50');
   expect(prices[2]).toHaveText('$22.50');
 
-  // Move Plus plan slider and check Plus plan price value has changed correctly
+  // Move Plus plan Users slider and check plan price value has changed correctly
   const sliders = await page.locator('[class="rc-slider rc-slider-horizontal"]').all();
   expect(sliders.length).toEqual(4);
-  const slider1box = await sliders[0].boundingBox();
-  if (!slider1box) {
+  const plusUserSlider = await sliders[0].boundingBox();
+  if (!plusUserSlider) {
     throw new Error('Unable to find bounding box on element');
   }
   await page.mouse.click(
-    slider1box!.x + slider1box!.width / 2, 
-    slider1box!.y + slider1box!.height / 2
+    plusUserSlider!.x + plusUserSlider!.width / 2, 
+    plusUserSlider!.y + plusUserSlider!.height / 2
   );
   expect(prices[0]).toHaveText('$9');
   expect(prices[1]).toHaveText('$81');
+  expect(prices[2]).toHaveText('$22.50');
+
+  // Move Plus plan Files silder and check plan price value has changed correctly
+  const plusFileSlider = await sliders[1].boundingBox();
+  if (!plusFileSlider) {
+    throw new Error('Unable to find bounding box on element');
+  }
+  await page.mouse.click(
+    plusFileSlider!.x + plusFileSlider!.width / 2, 
+    plusFileSlider!.y + plusFileSlider!.height / 2
+  );
+  expect(prices[0]).toHaveText('$9');
+  expect(prices[1]).toHaveText('$156.60');
   expect(prices[2]).toHaveText('$22.50');
 
   // Click Get started and check correct page
